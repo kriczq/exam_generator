0920f7edb7736ba7034f4909ad04da0e2af9033e
kriczq <kriczq@kriczqs-MacBook-Pro.local>
2019-09-25 10:46:18 +0200

    generate requirements.txt

--- flask_run.sh
+++ flask_run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+if [[ ! -d "venv" ]]; then
+    echo Creating virtualenv...
+    python3 -m venv venv
+fi
+
 source venv/bin/activate
+pip install -r requirements.txt
 
 export FLASK_APP=app/app.py
 
